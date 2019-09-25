21891153dd21d68b96f9dae9e784680baf63e702
Tarek <tug35668@temple.edu>
2019-09-25 12:51:43 -0400

    Added onItemSelectedListener to spinner

--- app/src/main/java/edu/temple/colorapp/ColorActivity.java
+++ app/src/main/java/edu/temple/colorapp/ColorActivity.java
@@ -2,8 +2,13 @@ package edu.temple.colorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 public class ColorActivity extends AppCompatActivity {
 
@@ -15,5 +20,19 @@ public class ColorActivity extends AppCompatActivity {
         ColorAdapter adapter = new ColorAdapter(this, colors);
         Spinner colorSpinner = (Spinner) findViewById(R.id.colorSpinner);
         colorSpinner.setAdapter(adapter);
+        colorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                adapterView.setBackgroundColor(Color.WHITE);
+                view.setBackgroundColor(Color.WHITE);
+                TextView selected = (TextView) view;
+                getWindow().getDecorView().setBackgroundColor(Color.parseColor(selected.getText().toString()));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
